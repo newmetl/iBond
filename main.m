4dbcb00fe697364723ddac608c07798a7de47de1
Adalbert Gorecki <adalbert.gorecki@9elements.com>
2011-03-19 16:42:40 +0100

    Setup XCode Project

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iBondXCode
+//
+//  Created by Adalbert Gorecki on 19.03.11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
